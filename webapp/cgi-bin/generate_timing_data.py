c6e220e6501e2e4c01b043c9d474d9f8e1e0957b
xiaofeng <xiao_feng@hunliji.com>
2017-09-26 18:31:50 +0800

    add new files

--- webapp/cgi-bin/generate_timing_data.py
+++ webapp/cgi-bin/generate_timing_data.py
@@ -1,16 +1,16 @@
 import cgi
 import athletemodel
-from athletelist import AthleteList
+import cgitb
+cgitb.enable()
 import yate
 athletelist = athletemodel.get_from_store()
 
 form_data = cgi.FieldStorage()
 athlete_name = form_data['which_athlete'].value
 
-athlete_times = AthleteList.top3(athletelist[athlete_name])
 print(yate.start_response())
 print(yate.include_header("Coach kelly's Timing Data"))
 print(yate.header("Athlete"+athlete_name+",DOB:"+athletelist[athlete_name].dob+"."))
 print(yate.para("The to times for this athlete are:"))
-print(yate.u_list(athletelist[athlete_name].top3()))
+print(yate.u_list(athletelist[athlete_name].top3))
 print(yate.include_footer({"Home":"/index.html","other athlete":"generate_list.py"}))
